426a845b9f28dd72277d38f42a927d9cdddf6a8d
Nurudeen Ajani <nurudeenolamide010@gmail.com>
2023-02-23 03:29:03 -0700

    God Abeg

--- Tandafood/settings.py
+++ Tandafood/settings.py
@@ -123,7 +123,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static/'),
 ]
 
-LOGIN_URL = 'login'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
